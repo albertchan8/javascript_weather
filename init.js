ad8aa148a4960d8400be6c0065593df792eb400c
Albert <ac564h@att.com>
2019-10-09 13:31:08 -0700

    weather project

---
+++ init.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var searchButton = document.querySelector("Button");
+var searchCity = document.querySelector("#city");
+
+var loadingText = document.querySelector("#load");
+var weatherBox = document.querySelector("#weather");
+
+var weatherCity = weatherBox.firstElementChild;
+var weatherDescription = document.querySelector("#weatherDescription");
+var weatherTemperature = document.querySelector("#weatherTemperature");
+
+
